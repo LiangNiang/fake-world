1ac5d1ea8179eeceff263ee1d1864172b4923c95
LiangNiang <liangniangbaby@gmail.com>
2024-01-26 10:58:34 +0800

    ✨ 自动添加好友增加限制

--- packages/web/src/components/RightPanel/QuickOperations.tsx
+++ packages/web/src/components/RightPanel/QuickOperations.tsx
@@ -1,6 +1,8 @@
 import { App, Button } from 'antd';
+import { getRecoil } from 'recoil-nexus';
 
 import { quickAddFakeUser, removeFakeUsers } from '@/faker/wechat/user';
+import { friendsIdsState } from '@/state/profile';
 
 const QuickOperations = () => {
   const { message } = App.useApp();
@@ -8,8 +10,14 @@ const QuickOperations = () => {
     <div className="flex space-x-1">
       <Button
         onClick={() => {
-          quickAddFakeUser(50);
-          message.success('添加成功');
+          requestIdleCallback(() => {
+            if (getRecoil(friendsIdsState).length > 300) {
+              message.error('好友数量超出限制，可在底部修改好友数量显示');
+              return;
+            }
+            quickAddFakeUser(50);
+            message.success('添加成功');
+          });
         }}
       >
         随机添加50个好友
