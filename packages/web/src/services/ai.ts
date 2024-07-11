6e7bd3d4b68f8d657d54054c20ab0c32019a87ca
LiangNiang <liangniangbaby@gmail.com>
2024-07-11 15:46:48 +0800

    🚨 Fix typing

--- packages/web/src/services/ai.ts
+++ packages/web/src/services/ai.ts
@@ -1,6 +1,5 @@
-import type { EConversationRole } from "@/state/conversationState";
 import type { AxiosRequestConfig } from "axios";
-import request, { type CommonJSONResponse } from "./request";
+import request from "./request";
 
 export async function generateChatMessage(props?: AxiosRequestConfig) {
 	return request.get("/api/v1/ai/chat_message", props);
